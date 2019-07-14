db381995fd68b34c2c7fbbf5e95dea4d6e6a37d1
Fazle <fazrbhyn@gmail.com>
2019-07-13 21:32:46 -0400

    Add updated src file

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Today's practice is to create an simple app using react and deploy on the Amazon AWS and Netlify cloud and 
+		  check-in code in GitHub.   
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
